00ad29f1f083fbd11babeafc8046f9db5b52a945
Sheriefalgalaly <109813086+Sheriefalgalaly@users.noreply.github.com>
2022-08-05 21:35:05 +0200

    Add files via upload

--- HAL/KeyPad/KeyPad_Program.c
+++ HAL/KeyPad/KeyPad_Program.c
@@ -22,10 +22,10 @@ void Delay_KeyPad(void){
 u8 HKeyPad_Pvoidvalueof_input(u8 x)
 {
 	switch(x){
-	case 8 : return 0;
-	case 4 : return 1;
-	case 2 : return 2;
-	case 1:  return 3;
+	case 3 :  return 1;
+	case 5 :  return 2;
+	case 6 :  return 3;
+	case 14:  return 0;
 	}
 }
 
@@ -34,20 +34,21 @@ u8 KeyPad_voidRead(KeyPad_t* key)
     u8 read_value =0;
     static u8 read_value_key=48;
     for (u8 Local_u8ColumnHigh=0;Local_u8ColumnHigh<NumOfColumns;Local_u8ColumnHigh++)
-    {
-    	    GPIO_PINWrite_High(*(key->PortColumn+Local_u8ColumnHigh),*(key->PinColumn+Local_u8ColumnHigh));
+    {       read_value=0;
+    	    GPIO_PINWrite_LOW(*(key->PortColumn+Local_u8ColumnHigh),*(key->PinColumn+Local_u8ColumnHigh));
     		for (u8 Local_u8Column=0;Local_u8Column<NumOfColumns;Local_u8Column++)
     		{
     			if (Local_u8ColumnHigh != Local_u8Column )
-    		    	  GPIO_PINWrite_LOW(*(key->PortColumn+Local_u8Column),*(key->PinColumn+Local_u8Column));
+    		    	  GPIO_PINWrite_High(*(key->PortColumn+Local_u8Column),*(key->PinColumn+Local_u8Column));
     		}
     		//read input
     		for (u8 Local_u8Raw=0;Local_u8Raw<NumOfRaws;Local_u8Raw++)
     		{
+
     			read_value |= GPIO_PINRead(*(key->PortRaw+Local_u8Raw),*(key->PinRaw+Local_u8Raw))<<Local_u8Raw;
     		}
 
-        if (read_value != 0 )
+        if (read_value != 7 )
         {
            read_value_key=KEYPAD[HKeyPad_Pvoidvalueof_input(read_value)][Local_u8ColumnHigh];                                 //if button pressed return value and out of function
            return read_value_key;
